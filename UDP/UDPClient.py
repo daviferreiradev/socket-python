0db9263550fcb99a79ad4b24798c15f83a323532
Davi Ferreira <daviferreiradev@gmail.com>
2024-10-27 19:36:35 -0300

    fix

--- UDP/UDPClient.py
+++ UDP/UDPClient.py
@@ -1,23 +1,16 @@
 from socket import * 
 
-# Define o nome do servidor e a porta para comunicação
-serverName = '127.0.0.1'  # Use 'localhost' ou IP do servidor
+serverName = '127.0.0.1'
 serverPort = 12000
 
-# Cria o socket do cliente utilizando UDP
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-# Lê uma mensagem do usuário para enviar ao servidor
 message = input('Input lowercase sentence: ')
 
-# Envia a mensagem codificada para o servidor
 clientSocket.sendto(message.encode(), (serverName, serverPort))
 
-# Recebe a resposta do servidor (tamanho máximo de 2048 bytes)
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 
-# Exibe a resposta do servidor
 print(f"Modified message from server: {modifiedMessage.decode()}")
 
-# Fecha o socket do cliente
 clientSocket.close()
--- UDP/UDPServer.py
+++ UDP/UDPServer.py
@@ -1,22 +1,15 @@
 from socket import *
 
-# Define a porta do servidor
 serverPort = 12000
 
-# Cria o socket do servidor utilizando UDP
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 
-# Associa o socket à porta para começar a escutar mensagens
 serverSocket.bind(('', serverPort))
 print("The server is ready to receive")
 
-# Loop infinito para receber mensagens dos clientes
 while True:
-    # Recebe uma mensagem do cliente (tamanho máximo de 2048 bytes)
     message, clientAddress = serverSocket.recvfrom(2048)
 
-    # Converte a mensagem para maiúsculas
     modifiedMessage = message.decode().upper()
 
-    # Envia a mensagem modificada de volta ao cliente
     serverSocket.sendto(modifiedMessage.encode(), clientAddress)
